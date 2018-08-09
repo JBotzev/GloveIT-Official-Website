30fa2e48ce8e1c0a4933746cbf052d1058a70bb3
Stanimir Bozhilov <stanimir.bozhilov.1998@gmail.com>
2018-08-09 14:45:34 +0200

    Added db_connection.php file in php folder

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,3 +1,9 @@
 <?php
 
+$dbname = "GloveITMailingList";
+$host = "localhost";
+$username = "root";
+$password = "89562332";
+$connection = new PDO("mysql:dbname=$dbname;host=$host", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
 ?>
